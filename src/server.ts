0c47d92e9d9d66bbaec2d33457bca10953433314
Marcelo-SmartEnvios <marcelo.guerreiro@smartenvios.com>
2022-09-28 09:31:55 -0300

    update env port

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,6 @@ import "dotenv/config";
 
 import { app } from "./app";
 
-app.listen(process.env.APP_PORT as unknown as number, () => {
-  console.log(`🤖 Server is running on port ${process.env.APP_PORT}`);
+app.listen(process.env.PORT, () => {
+  console.log(`🤖 Server is running on port ${process.env.PORT}`);
 });
