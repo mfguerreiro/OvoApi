fafbc3691e273e233e0f4b7b639cf0e3f290df02
mfguerreiro <mfguerreiro97@hotmail.com>
2022-10-12 11:57:15 -0300

    asd git push wefmlçml[] :quit q2jiooooooe
    wefnie
    
    fwmeif,dw
    dcw ?:E rq23
    
    '

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,15 @@
 import cors from "cors";
 import express from "express";
 import { router } from "./router";
+import swaggerUi from "swagger-ui-express";
+import swaggerDocument from "../swagger.json";
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 app.use(router);
+
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 export { app };
