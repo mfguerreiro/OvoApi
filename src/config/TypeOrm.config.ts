4a654d50ed6399416d5c3c569c4dc95ec32a5731
mfguerreiro <mfguerreiro97@hotmail.com>
2022-11-10 14:23:31 -0300

    change typeorm config

--- src/config/TypeOrm.config.ts
+++ src/config/TypeOrm.config.ts
@@ -1,12 +1,8 @@
 import { DataSource } from "typeorm";
 
 export const connection = new DataSource({
+  url: process.env.DATABASE_URL,
   type: "postgres",
-  host: process.env.POSTGRES_HOST,
-  port: process.env.POSTGRES_PORT as unknown as number,
-  username: process.env.POSTGRES_USER,
-  password: process.env.POSTGRES_PASSWORD,
-  database: process.env.POSTGRES_DATABASE,
   entities: ["src/**/*.entity.ts"],
   ssl: {
     rejectUnauthorized: false,
